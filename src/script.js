be8c5b94ec4f948f05ec6a9c95cf9e7adf40e42a
RemleyNick <remleynick@yahoo.com>
2023-04-28 19:40:15 +0000

    style new chat and clear inputs

--- src/script.js
+++ src/script.js
@@ -8,12 +8,16 @@ form.addEventListener('submit', function(event) {
   //target input values
   const name = document.getElementById('name').value;
   const message = document.getElementById('message').value;
-  
+
   //create alert
   alert(name + ": " + message);
 
   //add to chat history
   const addChat = document.createElement('div');
-  addChat.innerHTML = '<p>' + name + ': ' + message + '</p>';
+  addChat.innerHTML = '<p>' + '<strong>' + name + '</strong>' + ': ' + message + '</p>';
   chatHistory.appendChild(addChat);
+
+  //clear inputs
+  document.getElementById('name').value = "";
+  document.getElementById('message').value = "";
 });
