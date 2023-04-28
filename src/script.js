05d5465cb0d7d1916886c0f773bb00e278d59b25
RemleyNick <remleynick@yahoo.com>
2023-04-28 19:33:52 +0000

    add message to chat history

--- src/script.js
+++ src/script.js
@@ -1,7 +1,19 @@
 const form = document.querySelector('form');
+const chatHistory = document.getElementById('chat-history');
+
 form.addEventListener('submit', function(event) {
+  //prevent default submit
   event.preventDefault();
+
+  //target input values
   const name = document.getElementById('name').value;
   const message = document.getElementById('message').value;
+  
+  //create alert
   alert(name + ": " + message);
+
+  //add to chat history
+  const addChat = document.createElement('div');
+  addChat.innerHTML = '<p>' + name + ': ' + message + '</p>';
+  chatHistory.appendChild(addChat);
 });
